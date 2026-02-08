76f5fb3c0493d2f6e4745790dccf15831bcc36b4
Abraham <eshkim1@gmail.com>
2026-02-08 20:19:38 +0100

    modif muicModel

--- js/model/MusicModel.js
+++ js/model/MusicModel.js
@@ -4,21 +4,34 @@ export default class MusicModel {
     this.playlists = [];
   }
 
+  save() {
+    localStorage.setItem("playlists", JSON.stringify(this.playlists));
+  }
+
+  addSong(playlistName, genre, artist, song) {
+    let playlist = this.playlists.find((p) => p.name === playlistName);
+    if (!playlist) {
+      playlist = { name: playlistName, genres: [] };
+      this.playlists.push(playlist);
+    }
+
+    let genreObj = playlist.genres.find((g) => g.name === genre);
+    if (!genreObj) {
+      genreObj = { name: genre, artists: [] };
+      playlist.genres.push(genreObj);
+    }
+
+    let artistObj = genreObj.artists.find((a) => a.name === artist);
+    if (!artistObj) {
+      artistObj = { name: artist, songs: [] };
+      genreObj.artists.push(artistObj);
+    }
+
+    artistObj.songs.push(song);
+    this.save();
+  }
+
   getPlaylists() {
     return this.playlists;
   }
 }
-
-let genreObj = playlist.genres.find((g) => g.name === genre);
-if (!genreObj) {
-  genreObj = { name: genre, artists: [] };
-  playlist.genres.push(genreObj);
-}
-
-let artistObj = genreObj.artists.find((a) => a.name === artist);
-if (!artistObj) {
-  artistObj = { name: artist, songs: [] };
-  genreObj.artists.push(artistObj);
-}
-
-artistObj.songs.push(song);
