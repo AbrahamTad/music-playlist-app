13905d0f01ed3dd114a953ab6b8fa6ec73024425
Abraham <eshkim1@gmail.com>
2026-02-08 20:24:08 +0100

    modiefied app.js file

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ import MusicModel from "./model/MusicModel.js";
 import MusicView from "./view/MusicView.js";
 import MusicController from "./controller/MusicController.js";
 
-const model = new MusicModel();
-const view = new MusicView();
-new MusicController(model, view);
+
+window.addEventListener("load", () => {
+  const model = new MusicModel();
+  const view = new MusicView();
+  new MusicController(model, view);
+
+  //first render to show existing playlists
+  view.render(model.getPlaylists());
+});
