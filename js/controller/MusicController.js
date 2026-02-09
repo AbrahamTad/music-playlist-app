fbd803e739920e287a4a9ab143f9aee16051e197
Abraham <eshkim1@gmail.com>
2026-02-09 23:13:28 +0100

    update UI after adding song and reset form

--- js/controller/MusicController.js
+++ js/controller/MusicController.js
@@ -17,20 +17,10 @@ this.form.addEventListener("submit", (e) => {
   const artist = document.getElementById("artist").value.trim();
   const song = document.getElementById("song").value.trim();
 
-  // validation (prevent empty input)
-  if (!name || !genre || !artist || !song) {
-    alert("Alla fält måste fyllas i");
-    return;
-  }
-
-  // add to model
-  this.model.addSong(name, genre, artist, song);
-
-  // update UI
-  this.view.render(this.model.getPlaylists());
-
-  // clear form
-  this.form.reset();
+   this.model.addSong(name, genre, artist, song);
+   this.view.render(this.model.getPlaylists());
+   this.form.reset();
+  
 });
 
 
